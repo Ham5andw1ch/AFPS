d119e7578d440b5f744d7ebe18244c9a3628f855
William DeLaughter <williamd.delaughter@gmail.com>
2019-08-23 16:10:48 -0500

    Change movement

--- AFPS_Project/Assets/Scripts/MovementHandler.cs
+++ AFPS_Project/Assets/Scripts/MovementHandler.cs
@@ -16,43 +16,60 @@ public class MovementHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Mathf.Sin(controller.gameObject.transform.rotation.eulerAngles.y*(Mathf.PI/180)) + " " + Mathf.Cos(controller.gameObject.transform.rotation.eulerAngles.y)*(Mathf.PI/180));
-
-        if (Input.GetKey("w"))
-        {
-            xvel = speed * Mathf.Sin(controller.gameObject.transform.rotation.eulerAngles.y*(Mathf.PI/180));
-            zvel = speed * Mathf.Cos(controller.gameObject.transform.rotation.eulerAngles.y*Mathf.PI/180);
+        int[][] angles = new int[][]{
+            new int [] {-45 ,0  ,45},
+            new int [] {-90 ,-1 ,90},
+            new int [] {-135,180,135}
+        };
+        int xin = 1, yin =1;
+        if (Input.GetKey("w")){
+            xin--;
         }
         if (Input.GetKey("s"))
         {
-            xvel = speed * -Mathf.Sin(controller.gameObject.transform.rotation.eulerAngles.y*(Mathf.PI/180));
-            zvel = speed * -Mathf.Cos(controller.gameObject.transform.rotation.eulerAngles.y*Mathf.PI/180);
+            xin++;
         }
         if (Input.GetKey("d"))
         {
-            zvel =speed * -Mathf.Sin(controller.gameObject.transform.rotation.eulerAngles.y*(Mathf.PI/180));
-            xvel =speed * Mathf.Cos(controller.gameObject.transform.rotation.eulerAngles.y*Mathf.PI/180);
+            yin++;
         }
         if (Input.GetKey("a"))
         {
-            zvel =speed * Mathf.Sin(controller.gameObject.transform.rotation.eulerAngles.y*(Mathf.PI/180));
-            xvel =speed * -Mathf.Cos(controller.gameObject.transform.rotation.eulerAngles.y*Mathf.PI/180);
+            yin--;
         }
-        if (Mathf.Abs(xvel) < .1)
-        {
-            xvel = 0;
-        }
-        if (Mathf.Abs(zvel) < .1)
-        {
-            zvel = 0;
+        
+        Debug.Log(Mathf.Sin(controller.gameObject.transform.rotation.eulerAngles.y*(Mathf.PI/180)) + " " + Mathf.Cos(controller.gameObject.transform.rotation.eulerAngles.y)*(Mathf.PI/180));
+        if(angles[xin][yin] != -1){
+            xvel = speed * Mathf.Sin((controller.gameObject.transform.rotation.eulerAngles.y+angles[xin][yin])*(Mathf.PI/180));
+            zvel = speed * Mathf.Cos((controller.gameObject.transform.rotation.eulerAngles.y+angles[xin][yin])*Mathf.PI/180);
+
         }
         if (controller.isGrounded)
         {
-            xvel += xvel / -friction;
-            zvel += zvel / -friction;
-            //            Debug.Log("Yikes");
+                float oldx = xvel;
+                float oldz = zvel;
+                if(xvel != 0)
+                xvel += -xvel/Mathf.Abs(xvel) * friction;
+                if(zvel != 0)
+                zvel += -zvel/Mathf.Abs(zvel) * friction;
+                Debug.Log("Vels: " + oldx + " " + xvel);
+                if (oldx/xvel <= 0)
+                    xvel = 0;
+                if (oldz/zvel <= 0)
+                    zvel = 0;
 
+                //            Debug.Log("Yikes");
+            
+            if (Mathf.Abs(xvel) < .001)
+            {
+                xvel = 0;
+            }
+            if (Mathf.Abs(zvel) < .001)
+            {
+                zvel = 0;
+            }
         }
+
         else
         {
             yvel = yvel - 9.8f * Time.deltaTime;
