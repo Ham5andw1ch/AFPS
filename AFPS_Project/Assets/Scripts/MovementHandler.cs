426f89f9aedfeb261b1e7597f4c223219c9788a6
William DeLaughter <williamd.delaughter@gmail.com>
2019-08-28 15:15:44 -0500

    Added jump queing, and fixed friction for good.

--- AFPS_Project/Assets/Scripts/MovementHandler.cs
+++ AFPS_Project/Assets/Scripts/MovementHandler.cs
@@ -5,65 +5,114 @@ public class MovementHandler : MonoBehaviour
 {
     public CharacterController controller;
     private float xvel, yvel, zvel;
-    public float speed;
+    public float speed, accSpeed, gravity, jvel;
+    private bool crouching = false, walking = false, jumped = false, jumpQueue = false;
     public float friction;
     // Start is called before the first frame update
     void Start()
     {
-
+        crouching = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        int[][] angles = new int[][]{
+
+        if (controller.isGrounded)
+        {
+            jumped = false;
+            yvel = 0;
+            float oldx = xvel;
+            float oldz = zvel;
+            float curSpeed = Mathf.Sqrt(oldx * oldx + oldz * oldz);
+
+            float velSpeed = accSpeed * Time.deltaTime;
+            int[][] angles = new int[][]{
             new int [] {-45 ,0  ,45},
             new int [] {-90 ,-1 ,90},
             new int [] {-135,180,135}
         };
-        int xin = 1, yin =1;
-        if (Input.GetKey("w")){
-            xin--;
-        }
-        if (Input.GetKey("s"))
-        {
-            xin++;
-        }
-        if (Input.GetKey("d"))
-        {
-            yin++;
-        }
-        if (Input.GetKey("a"))
-        {
-            yin--;
-        }
-        
-        Debug.Log(Mathf.Sin(controller.gameObject.transform.rotation.eulerAngles.y*(Mathf.PI/180)) + " " + Mathf.Cos(controller.gameObject.transform.rotation.eulerAngles.y)*(Mathf.PI/180));
-        if(angles[xin][yin] != -1){
-            xvel = speed * Mathf.Sin((controller.gameObject.transform.rotation.eulerAngles.y+angles[xin][yin])*(Mathf.PI/180));
-            zvel = speed * Mathf.Cos((controller.gameObject.transform.rotation.eulerAngles.y+angles[xin][yin])*Mathf.PI/180);
+            int xin = 1, yin = 1;
+            if (Input.GetKey("w"))
+            {
+                xin--;
+            }
+            if (Input.GetKey("s"))
+            {
+                xin++;
+            }
+            if (Input.GetKey("d"))
+            {
+                yin++;
+            }
+            if (Input.GetKey("a"))
+            {
+                yin--;
+            }
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                if (crouching == false)
+                {
+                    crouching = true;
+                    velSpeed = curSpeed * .2f;
+                }
+            }
+            else
+            {
+                crouching = false;
+            }
 
-        }
-        if (controller.isGrounded)
-        {
-                float oldx = xvel;
-                float oldz = zvel;
-                float oldspeed = Mathf.Sqrt(oldx*oldx+oldz*oldz);
-                float xf =(-xvel/Mathf.Abs(xvel) * friction * xvel)/oldspeed;
-                float zf =(-xvel/Mathf.Abs(zvel) * friction * zvel)/oldspeed;
-                Debug.Log("fs: " + xf + " " + zf + " " + oldspeed);
-                if(xvel != 0 && oldspeed != 0)
-                xvel += (-xvel * friction)/oldspeed;
-                if(zvel != 0 && oldspeed != 0)
-                zvel += (-zvel * friction) /oldspeed;
-                Debug.Log("Vels: " + oldx + " " + oldz + " " + oldspeed);
-                if (oldx/xvel <= 0)
-                    xvel = 0;
-                if (oldz/zvel <= 0)
-                    zvel = 0;
+            Debug.Log(curSpeed + " " + accSpeed + " " + speed);
+            if ((angles[xin][yin] != -1))
+            {
+                float newx = xvel + velSpeed * Mathf.Sin((controller.gameObject.transform.rotation.eulerAngles.y + angles[xin][yin]) * (Mathf.PI / 180));
+                float newz = zvel + velSpeed * Mathf.Cos((controller.gameObject.transform.rotation.eulerAngles.y + angles[xin][yin]) * Mathf.PI / 180);
+                if (!(Mathf.Sqrt(newx * newx + newz * newz) < speed))
+                {
+                    xvel = speed * Mathf.Sin((controller.gameObject.transform.rotation.eulerAngles.y + angles[xin][yin]) * (Mathf.PI / 180));
+                    zvel = speed * Mathf.Cos((controller.gameObject.transform.rotation.eulerAngles.y + angles[xin][yin]) * Mathf.PI / 180);
+                }
+                else
+                {
+                    xvel = newx;
+                    zvel = newz;
+                }
 
-                //            Debug.Log("Yikes");
-            
+
+                Debug.Log("Vels: " + xvel + " " + zvel);
+
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if(!jumped && jumpQueue){
+                jumpQueue = false;
+                jumped = true;
+                Debug.Log("Adding lul");
+                yvel += jvel;
+                }
+            } else{
+                jumpQueue = true;
+            }
+            oldx = xvel;
+            oldz = zvel;
+            Debug.Log("speed Before Friction:" + curSpeed + "\toldspeed After Friction:" + Mathf.Sqrt(xvel * xvel + zvel * zvel));
+            float oldspeed = Mathf.Sqrt(xvel * xvel + zvel * zvel);
+            float xf = (-xvel / Mathf.Abs(xvel) * friction * Time.deltaTime * xvel) / oldspeed;
+            float zf = (-zvel / Mathf.Abs(zvel) * friction * Time.deltaTime * zvel) / oldspeed;
+            Debug.Log("fs: " + xf + " " + zf + " " + oldspeed);
+            if (xvel != 0 && oldspeed != 0)
+                xvel += (-xvel * friction * Time.deltaTime) / oldspeed;
+            if (zvel != 0 && oldspeed != 0)
+                zvel += (-zvel * friction * Time.deltaTime) / oldspeed;
+            //Debug.Log("Vels: " + oldx + " " + oldz + " " + oldspeed);
+            if (oldx / xvel <= 0)
+                xvel = 0;
+            if (oldz / zvel <= 0)
+                zvel = 0;
+            Debug.Log("Speed Before Friction:" + oldspeed + "\tSpeed After Friction:" + Mathf.Sqrt(xvel * xvel + zvel * zvel));
+
+            //            Debug.Log("Yikes");
+
             //if (Mathf.Abs(xvel) < .001)
             //{
             //    xvel = 0;
@@ -76,11 +125,15 @@ public class MovementHandler : MonoBehaviour
 
         else
         {
-            yvel = yvel - 9.8f * Time.deltaTime;
-            //            Debug.Log("Yikes2");
+            if (!Input.GetKey(KeyCode.Space))
+            {
+                jumpQueue = true;
+            }   
+            yvel = yvel - gravity * Time.deltaTime;
+            Debug.Log("yvel " + yvel);
         }
-        controller.Move(new Vector3(xvel, yvel, zvel));
+        controller.Move(new Vector3(xvel * Time.deltaTime, yvel * Time.deltaTime, zvel * Time.deltaTime));
         //Debug.Log(controller.gameObject.transform.position);
-              Debug.Log(xvel + " " + yvel + " " + zvel);
+        Debug.Log(xvel + " " + yvel + " " + zvel);
     }
 }
